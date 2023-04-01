10e26471774bbb0639a5b113411a1711eab879a8
Laxmireddy <117433906+laxmireddy77@users.noreply.github.com>
2023-04-01 11:20:03 +0530

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const cors=require("cors");
 const path=require("path");
 const app=express();
 app.use(cors());
-app.use(express.static(path.join(__dirname),"./client/build"));
+app.use(express.static(path.join(__dirname,"./client/build")));
 app .get("/getEmployees",async (req,res)=>{
 let fetchedData= await employee.find();
 console.log(fetchedData);
